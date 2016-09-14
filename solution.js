df0b836649344f1f594a3e16b619fde0515ea130
saposki <saposki@gmail.com>
2016-09-14 14:16:32 -0700

    completed SearchBar

--- solution.js
+++ solution.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
-export const FilterableProductTable = React.creatClass({
+export const FilterableProductTable = React.createClass({
+  render() {
+    return (
+      <SearchBar/>
+    )
+  }
+})
+
+export const SearchBar = React.createClass({
   render() {
     return (
       <div></div>
